e85e602b048dbd282cf061db5eb9db82237f04f6
judlejudle <judean2021@gmail.com>
2022-11-03 07:39:24 +0900

    created header

---
+++ src/components/Root.tsx
@@ -0,0 +1,18 @@
+import { Box, Button, HStack } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import { FaAirbnb } from 'react-icons/fa'
+export default function Root() {
+    return (
+    <Box>
+        <HStack justifyContent={"space-between"} py ={5} px={10} borderBottomWidth={1}>
+            <Box color = "red.500">
+                <FaAirbnb size={"48"} />
+            </Box>
+            <HStack spacing={2}>
+                <Button> Log In</Button>
+                <Button colorScheme={"red"}> Sign up</Button>
+            </HStack>
+        </HStack>
+    </Box>
+    )
+}
